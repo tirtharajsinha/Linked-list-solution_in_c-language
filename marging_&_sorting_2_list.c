f9d42bb991adc92822c3f0a86e13404ab6a89ec6
Tirtharaj Sinha <64787936+tsidealONEidentity@users.noreply.github.com>
2020-08-05 21:50:12 +0530

    Create marging_&_sorting_2_list.c

---
+++ marging_&_sorting_2_list.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+struct node{  
+    int data;  
+    struct node *next;  
+};      
+   
+//Represent the head and tail of the singly linked list  
+struct node *head, *tail = NULL;  
+   
+//addNode() will add a new node to the list  
+void addNode(int data) {  
+    //Create a new node  
+    struct node *newNode = (struct node * )malloc( sizeof(struct node));  
+    newNode->data = data;  
+    newNode->next = NULL;  
+      
+    //Checks if the list is empty  
+    if(head == NULL) {  
+        //If list is empty, both head and tail will point to new node  
+        head = newNode;  
+        tail = newNode;  
+    }  
+    else {  
+        //newNode will be added after tail such that tail's next will point to newNode  
+        tail->next = newNode;  
+        //newNode will become new tail of the list  
+        tail = newNode;  
+    }  
+}  
+void sortList() {  
+        //Node current will point to head  
+        struct node *current = head, *index = NULL;  
+        int temp;  
+          
+        if(head == NULL) {  
+            return;  
+        }  
+        else {  
+            while(current != NULL) {  
+                //Node index will point to node next to current  
+                index = current->next;  
+                  
+                while(index != NULL) {  
+                    //If current node's data is greater than index's node data, swap the data between them  
+                    if(current->data > index->data) {  
+                        temp = current->data;  
+                        current->data = index->data;  
+                        index->data = temp;  
+                    }  
+                    index = index->next;  
+                }  
+                current = current->next;  
+            }      
+        }  
+    }  
+   
+//display() will display all the nodes present in the list  
+void display() {  
+    //Node current will point to head  
+    struct node *current = head;  
+    if(head == NULL) {  
+        printf("List is empty \n");  
+        return;  
+    }  
+    while(current != NULL) {  
+        //Prints each node by incrementing pointer  
+        printf("%d ", current->data);  
+        current = current->next;  
+    }  
+    printf("\n");  
+}  
+      
+int main() {
+
+    int n,m,data;
+    scanf("%d%d",&n,&m);
+    for(int i=0;i<n+m;i++)
+   {
+       scanf("%d",&data);
+       addNode(data);
+   }
+    sortList();
+    display();
+    
+    return 0;
+}
